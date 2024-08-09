92c558f5ead42317d76721e0ce4647fd1a44d3ff
Michal Hladík <michalhladik2@gmail.com>
2024-08-09 12:04:40 +0200

    změna ve skriptu

--- start_services.sh
+++ start_services.sh
@@ -5,6 +5,6 @@ ROOT_PATH="/home/admin/intranet"
 # Start Node.js Express proxy server
 echo "Starting Node.js Express proxy server"
 cd $ROOT_PATH/main
-npm install
-wait
 nohup sudo npm run start &
+
+echo "Everything started!"
