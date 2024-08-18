3402a37e71c62eafc5c4e4310dbb09c09f9a353b
Michal Hladík <michalhladik2@gmail.com>
2024-08-18 18:58:19 +0200

    malý seznam receptů na hlavní stránce

--- kucharka/src/app/page.tsx
+++ kucharka/src/app/page.tsx
@@ -6,9 +6,35 @@ export default async function Home() {
   return (
     <>
       <Header text="Seznam receptů" />
-      {data.map((e) => (
-        <p key={e.id}>{e.title}</p>
-      ))}
+      <section className="flex flex-col items-center">
+        {data.map((e) => (
+          <div key={e.id} className="p-5 text-lime-200 flex flex-col gap-2">
+            <h3 className="text-lg font-bold uppercase">{e.title}</h3>
+            <p>Popis:</p>
+            <p>{e.content}</p>
+
+            <p>Čas přípravy je {e.totalTimeInMinutes} minut</p>
+            <table>
+              <thead>
+                <th>
+                  <td>Ingredience:</td>
+                </th>
+              </thead>
+              <tbody>
+                {e.ingredientToRecipe.map((i) => (
+                  <tr key={i.ingredient.id}>
+                    <td>{i.ingredient.name}</td>
+                    <td>
+                      {i.ingredientAmount} {i.ingredientAmountType}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+            <p>Tagy: {e.TagToRecipe.map((t) => t.tag.name).join(", ")}</p>
+          </div>
+        ))}
+      </section>
     </>
   );
 }
--- kucharka/src/utils/database/getRecipeData.ts
+++ kucharka/src/utils/database/getRecipeData.ts
@@ -3,11 +3,27 @@
 import prisma from "@/lib/prisma";
 
 export const getRecipeData = async () => {
+  console.log(
+    await prisma.recipe.findMany({
+      include: {
+        ingredientToRecipe: true,
+        TagToRecipe: true,
+      },
+    })
+  );
+
   const data = await prisma.recipe.findMany({
     include: {
       TagToRecipe: {
         select: {
           tag: true,
+        },
+      },
+      ingredientToRecipe: {
+        select: {
+          ingredient: true,
+          ingredientAmount: true,
+          ingredientAmountType: true,
         },
       },
     },
